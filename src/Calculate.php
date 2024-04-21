dd0296d30fe35a3d25e96d68d3b09fa969ce25c4
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-04-21 21:20:35 +0900

    add step2

--- src/Calculate.php
+++ src/Calculate.php
@@ -1,8 +1,6 @@
 <?php
-
 class Calculate
 {
-
   public function calculateTotalValue(array $hands, int $total_value): int
   {
     foreach ($hands as $hand) {
@@ -10,13 +8,32 @@ class Calculate
 
       if ($value == 'J' || $value == 'Q' || $value == 'K') {
         $total_value += 10;
-      } else if ($value == "A") {
-        $total_value += 1;
+      } else if ($value == 'A') {
+        $total_value = $this->calculateAceValue($total_value);
       } else {
         $total_value += intval($value);
       }
     }
 
     return $total_value;
+  }
+
+  private function calculateAceValue(int $total_value): int
+  {
+    $add_one = $total_value + 1;
+    $add_eleven = $total_value + 11;
+
+    if (21 < $add_one) {
+      return $add_one;
+    }
+
+    $add_one_diff = abs(21 - $add_one);
+    $add_eleven_diff = abs(21 - $add_eleven);
+
+    if ($add_one_diff < $add_eleven_diff) {
+      return $add_one;
+    } else if ($add_eleven_diff < $add_one_diff) {
+      return $add_eleven;
+    }
   }
 }
